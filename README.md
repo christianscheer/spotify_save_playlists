2c1da01b7b64847b369b6f9801f4c858f3e96269
Christian Scheer <33838285+christianscheer@users.noreply.github.com>
2018-05-08 19:34:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # spotify_save_playlists
+
+This is a python script that I use to save my spotify-playlists into json files.
+
+## Requirements
+
+- spotipy 2.4.4
+
+Spotipy can be installed via pip with the following command:
+
+``` bat
+pip install spotipy
+```
+
+- a registered spotify app
+
+If you want to use this you need to head to https://beta.developer.spotify.com/dashboard/ and register/create your own spotify app. This is how you obtain/set your own ```client_id```, ```client_secret``` and ```redirect_uri```.
+
+## Usage
+
+``` bat
+python app.py [client_id] [client_secret] [redirect_uri] [user_name] [target_folder]
+```
+
+```user_name``` is your spotify-user-name.
+
+When you run this for the first time you will be prompted(in the browser) to login with your spotify credentials and give access to your spotify app.
---
+++ app.py
@@ -0,0 +1,133 @@
+import spotipy
+import spotipy.util as util
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
+import string
+import os
+import sys
+
+
+def get_spotify_token(user_name, client_id, client_secret, redirect_uri):
+    scope = "user-library-read playlist-read-private playlist-read-collaborative"
+    token = util.prompt_for_user_token(
+        user_name, scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri)
+    return token
+
+
+def format_item(item):
+    track = item["track"]
+    spotify_id = track["id"]
+    artists = [ai["name"] for ai in track["artists"]]
+    name = track["name"]
+    return {"spotify_id": spotify_id,
+            "artists": artists, "name": name}
+
+
+def get_saved_tracks(spotify):
+    result = []
+    chunk_size = 50
+    offset = 0
+    results = spotify.current_user_saved_tracks(limit=50, offset=offset)
+    while results["items"]:
+        result.extend([format_item(item) for item in results["items"]])
+        offset += chunk_size
+        results = spotify.current_user_saved_tracks(
+            limit=chunk_size, offset=offset)
+    return result
+
+
+def get_playlist_tracks(spotify, owner_id, playlist_id):
+    result = []
+    pl = None
+    tracks = None
+
+    try:
+        pl = spotify.user_playlist(owner_id, playlist_id)
+        tracks = pl["tracks"]
+        result.extend([format_item(ti) for ti in tracks["items"]])
+    except:
+        pass
+
+    while tracks and tracks["next"]:
+        try:
+            tracks = spotify.next(tracks)
+            result.extend([format_item(ti) for ti in tracks["items"]])
+        except:
+            pass
+
+    return result
+
+
+def get_playlists(spotify):
+    chunk_size = 50
+    offset = 0
+    pls = spotify.current_user_playlists(limit=chunk_size, offset=offset)
+    while pls["items"]:
+        for pl in pls["items"]:
+            playlist_id = pl["id"]
+            name = pl["name"]
+            owner_id = pl["owner"]["id"]
+            tracks = get_playlist_tracks(spotify, owner_id, playlist_id)
+            if tracks:
+                playlist = {"playlist_id": playlist_id,
+                            "name": name,
+                            "tracks": tracks}
+                # result.append(playlist)
+                yield playlist
+        offset += chunk_size
+        pls = spotify.current_user_playlists(
+            limit=chunk_size, offset=offset)
+    # return result
+
+
+def format_filename(s):
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    filename = "".join(c for c in s if c in valid_chars)
+    return filename
+
+
+def write_to_json(file_name, object):
+    with open(file_name, "wt", encoding="utf-8") as outfile:
+        json.dump(object, outfile)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 6:
+        print("invalid arguments!")
+        print(
+            "expected arguments: [client_id] [client_secret] [redirect_uri] [user_name] [target_folder]")
+    else:
+        client_id = sys.argv[1]
+        client_secret = sys.argv[2]
+        redirect_uri = sys.argv[3]
+        user_name = sys.argv[4]
+        target_folder = sys.argv[5]
+        print("using '%s' as client_id." % client_id)
+        print("using '%s' as client_secret." % client_secret)
+        print("using '%s' as redirect_uri." % redirect_uri)
+        print("using '%s' as user_name." % user_name)
+        print("using '%s' as target_folder." % target_folder)
+
+        token = get_spotify_token(user_name, client_id,
+                                  client_secret, redirect_uri)
+
+        sp = spotipy.Spotify(auth=token)
+        sp.trace = False
+
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+
+        print("getting saved tracks")
+        saved_tracks = get_saved_tracks(sp)
+        saved_tracks_filename = os.path.join(
+            target_folder, "saved_tracks.json")
+        write_to_json(saved_tracks_filename, saved_tracks)
+        print("wrote saved tracks to %s" % saved_tracks_filename)
+
+        print("getting playlists")
+        for pl in get_playlists(sp):
+            playlist_filename = os.path.join(
+                target_folder, "%s.json") % format_filename(pl["name"])
+            write_to_json(playlist_filename, pl)
+            print("written playlist to %s" % playlist_filename)
